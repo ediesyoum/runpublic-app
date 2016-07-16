52613730151bd04be7be8c000ad7d9a128edd513
jtkaufman737 <jtkaufman737@gmail.com>
2016-07-16 12:35:38 -0400

    Add stopwatch buttons not yet functional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,27 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def about
-  end
+    def time_start_calc
+      @time=Time.now
+      @start_hour=@time.hour
+      @start_minute=@time.min
+      @start_second=@time.sec
+      puts @time.strftime("%H:%M:%S")
+    end
 
+    def time_end_calc
+     @time_end=Time.now
+     @end_hour=@time_end.hour
+     @end_minutes=@time_end.min
+     @end_seconds=@time_end.sec
+     @time_end.strftime("%H:%M:%S")
+    end
+
+    def total_time
+     total_hour = (@end_hour.to_i) - (@start_hour.to_i)
+     total_minutes = (@end_minutes.to_i) - (@start_minute.to_i)
+     total_seconds = (@end_seconds.to_i) - (@start_second.to_i)
+     print total_hour.to_s + ":" + total_minutes.to_s + ":" + total_seconds.to_s
+   end
 end
+end 
