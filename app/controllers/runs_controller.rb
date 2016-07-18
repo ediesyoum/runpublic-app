01634f6544045158c8d72e7082e832c64d17a901
Eden Syoum <eden@temple.edu>
2016-07-18 09:31:48 -0400

    small changes to the nav and root page

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -5,31 +5,15 @@ class RunsController < ApplicationController
   end
 
   def show
-<<<<<<< HEAD
-    @run = Run.new
-=======
     @run = Run.find(params[:id])
->>>>>>> faf230fa3390b8da1322bb87bc7e1d1665662174
     # @comments = @run.comments
   end
 
   def new
-<<<<<<< HEAD
-<<<<<<< HEAD
-    # puts request.params
-    # @comment = Comment.find(params[:comment_id])
-    # @run = Run.new
-=======
-    puts request.params
-    @comment = Comment.find(params[:comment_id])
-    @run = Run.new
->>>>>>> ad9b0737825d0cc268fe285640b99fb49ca616e3
-=======
     # puts request.params
     @run = Run.new
     # @comment = Comment.find(params[:comment_id])
     redirect_to new_run_path
->>>>>>> faf230fa3390b8da1322bb87bc7e1d1665662174
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,7 @@ Rails.application.routes.draw do
 
   get "runs", to: "runs#index"
   get "runs/:id", to: "runs#show", as: :run
-<<<<<<< HEAD
-
-  get "runs/new", to: "runs#create"
-
-=======
   get "runs/new", to: "runs#new"
->>>>>>> ad9b0737825d0cc268fe285640b99fb49ca616e3
   get "runs/:id/edit", to: "runs#edit", as: :run_edit
 
   patch "runs/:id", to: "runs#update"
